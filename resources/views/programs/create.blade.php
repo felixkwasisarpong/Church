ede133ce4f43207d2ed96dd813245c871b8c1706
Apple <Apple@Felixs-Mac.local>
2024-01-16 21:15:19 -0600

    test push for git actions

--- resources/views/programs/create.blade.php
+++ resources/views/programs/create.blade.php
@@ -20,7 +20,7 @@
 
 <div class="alert alert-danger">
 
-    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <strong>Whoops!</strong> There were some problems with your input<br><br>
 
     <ul>
 
